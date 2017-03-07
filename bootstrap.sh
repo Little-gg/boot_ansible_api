8f776e9bfaac53f3e98e4e63cb70ba9671ff5674
GuanJi Chen <chenguanji@vanecloud.com>
2017-03-07 05:15:31 +0000

    v0.1

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum groupinstall "Development Tools" -y
+yum install python-devel openssl-devel git -y
+
+cd /tmp
+curl -O https://bootstrap.pypa.io/get-pip.py
+python /tmp/get-pip.py
+
+cd ~/
+git clone https://github.com/Little-gg/kree
+cd kree
+pip install -r requirements.txt
+ansible-playbook install.yml
